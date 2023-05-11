08590d952604eaa3aa07b85cfe5362ebb43dcece
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-05-11 13:56:01 +0530

    authentication

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const URL = process.env.URL
 const DB = "node-mongo-connect";
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
-
+const {add} = require('./custome-package')
+let result = add(2,3)
 
 const users = []
 //middleware
@@ -27,81 +28,6 @@ app.get('/about', function (req, res) {
     res.json({ message: "About...." })
 })
 
-// app.post("/product", async function (req, res) {
-
-//    try{
-//     const connection = await mongoClient.connect(URL)
-//     const db = connection.db(DB)
-//     await db.collection("products").insertOne(req.body)
-//     await connection.close()
-//     res.status(200).json({message:"Data Inserted"})
-//    }
-//    catch(error){
-//     console.log(error)
-//     res.status(500).json({message:"Something went wrong"})
-//    }
-    
-// })
-
-// app.get('/products', async function (req, res) {
-    
-//     try{
-//         const connection = await mongoClient.connect(URL)
-//         const db = connection.db(DB)
-//       let user = await db.collection("products").find().toArray()
-//         await connection.close()
-//         // res.status(200).json({message:"Got data "})
-//         res.json(user)
-//     }
-//     catch(error){
-//         console.log(error)
-//         res.status(500).json({message:"Something went wrong"})
-//        }
-
-//     res.json(users)
-// })
-
-// app.get("/product/:id", async function (req, res) {
-//     try{
-//         const connection = await mongoClient.connect(URL)
-//         const db = connection.db(DB)
-//       let resUser = await db.collection("products").findOne({_id: new mongodb.ObjectId(req.params.id)})
-//         await connection.close()
-        
-//         res.json(resUser)
-//     }
-//     catch(error){
-//         console.log(error)
-//         res.status(500).json({message:"Something went wrong"})
-//        }
-// })
-
-// app.put("/product/:id",async function (req, res) {
-//     try{
-//         const connection = await mongoClient.connect(URL)
-//         const db = connection.db(DB)
-//       let resUser = await db.collection("products").findOneAndUpdate({_id: new mongodb.ObjectId(req.params.id)},{$set:req.body})
-//         await connection.close()
-//         res.json(resUser)
-//     }
-//     catch(error){
-//         console.log(error)
-//         res.status(500).json({message:"Something went wrong"})
-//        }
-// })
-// app.delete('/product/:id',async function (req, res) {
-//     try{
-//         const connection = await mongoClient.connect(URL)
-//         const db = connection.db(DB)
-//       let resUser = await db.collection("products").findOneAndDelete({_id: new mongodb.ObjectId(req.params.id)})
-//         await connection.close()
-//         res.json(resUser)
-//     }
-//     catch(error){
-//         console.log(error)
-//         res.status(500).json({message:"Something went wrong"})
-//        }
-// })
 
 app.post("/user", async function (req, res) {
 
@@ -110,7 +36,7 @@ app.post("/user", async function (req, res) {
         const db = connection.db(DB)
         await db.collection("users-info").insertOne(req.body)
         await connection.close()
-        res.status(200).json({message:"Data Inserted"})
+      return  res.status(200).json({message:"Data Inserted"})
        }
        catch(error){
         console.log(error)
@@ -120,17 +46,17 @@ app.post("/user", async function (req, res) {
     })
 
     
-    let authenticate = function(req,res,next){
+    let authenticate = async function(req,res,next){
         console.log(req.headers)
         if(req.headers.authorization){
             try{
-                const decode = jwt.verify(req.headers.authorization,process.env.SECRET)
+                const decode = await jwt.verify(req.headers.authorization,process.env.SECRET)
                 if(decode){
                     next()
                 }
             }
             catch(error){
-                res.status(401).json({message:"Unauthorized"})
+              return  res.status(401).json({message:"Unauthorized"})
             }
         }else{
             res.status(401).json({message:"Unauthorized"})
@@ -145,14 +71,14 @@ app.post("/user", async function (req, res) {
               let user = await db.collection("users-info").find().toArray()
                 await connection.close()
                 // res.status(200).json({message:"Got data "})
-                res.json(user)
+              return  res.json(user)
             }
             catch(error){
                 console.log(error)
                 res.status(500).json({message:"Something went wrong"})
                }
         
-            res.json(users)
+          return  res.json(users)
         })
 
 
@@ -163,11 +89,11 @@ app.post("/user", async function (req, res) {
       let resUser = await db.collection("users-info").findOne({_id: new mongodb.ObjectId(req.params.id)})
         await connection.close()
         
-    res.json(resUser)
+      return  res.json(resUser)
     }
     catch(error){
         console.log(error)
-        res.status(500).json({message:"Something went wrong"})
+      return  res.status(500).json({message:"Something went wrong"})
        }
 })
 
@@ -177,11 +103,11 @@ app.put("/user/:id",async function (req, res) {
         const db = connection.db(DB)
       let resUser = await db.collection("users-info").findOneAndUpdate({_id: new mongodb.ObjectId(req.params.id)},{$set:req.body})
         await connection.close()
-        res.json(resUser)
+      return  res.json(resUser)
     }
     catch(error){
         console.log(error)
-        res.status(500).json({message:"Something went wrong"})
+      return  res.status(500).json({message:"Something went wrong"})
        }
 })
 
@@ -191,11 +117,11 @@ app.delete('/user/:id',async function (req, res) {
         const db = connection.db(DB)
       let resUser = await db.collection("users-info").findOneAndDelete({_id: new mongodb.ObjectId(req.params.id)})
         await connection.close()
-        res.json(resUser)
+      return  res.json(resUser)
     }
     catch(error){
         console.log(error)
-        res.status(500).json({message:"Something went wrong"})
+      return  res.status(500).json({message:"Something went wrong"})
        }
 })
 
@@ -227,17 +153,17 @@ app.post('/login',async function(req,res){
             if (compare) {
                 const token = jwt.sign({_id:user._id},process.env.SECRET,{expiresIn:"10m"})
                 console.log(token)
-                res.json({token})
+              return  res.json({token})
 
             } else {
-                res.json({message:"Enter correct Email/Password"})
+              return  res.json({message:"Enter correct Email/Password"})
             }
         } else {
             res.status(401).json({message:"Email/password not found"})
         }
         await connection.close();
     } catch (error) {
-        res.status(500).json({message:"Something Went Wrong"})
+      return  res.status(500).json({message:"Something Went Wrong"})
     }
 })
 
